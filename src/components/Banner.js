f0033916650bcae9bc4730517580d0ea60387482
Deen Mohammad Abdullah <rabi.mist@gmail.com>
2023-11-22 08:06:04 -0700

    update about info

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,24 @@ export const Banner = () => {
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Deen`} </h1>
                 <h2><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "NLP Engineer", "Software Engineer", "Data Science Specialist", "Cloud Practitioner" ]'><span className="wrap">{text}</span></span></h2>
-                  <p> Hi, I am Deen, pursuing my Ph.D. research (as an Alberta Innovates Scholar) on Machine Learning and Software Engineering at the University of Lethbridge. I am experienced in working with the SOTA NLP models (Transformer, BERT, GPT, BART, GCN, GAT) to improve the performance of the Query-Focused Summarization Models. Passionate about research and competitive programming, where I love to play with data structures and algorithms to find the optimal solution. Languages that I prefer to work with are Python, C++, JAVA, C#, and JavaScript. I am also interested in cloud platforms, data science and deep learning models to develop ML models and find the key insights from the crazy data.  </p>
+<p> 
+  I am an active learner and passionate about learning new technologies and tools.
+</p>
+<p>
+  Currently, I am working as a researcher at the University of Lethbridge, where my research area includes analysis of programming languages to determine how the programming approaches or pattern differs based on demographics such as gender, age, region, and expertise of a programmer. I am using machine learning approaches to identify whether sociolinguistic factors and software metrics can determine the programming approaches of a programmer. To conduct my research, I have received the Alberta Innovates Scholarship ($31000/year for four years) from Alberta Innovates. 
+</p>
+<p>
+  I completed my M.Sc. from the University of Lethbridge, majoring in Natural Language Processing (NLP). During my research, I worked on query-focused summarization tasks using the BERT-SUM model and published the work in one of the conferences (INLG) of ACL. Besides, I have hands-on experience using different ML models, including BERT, BART, Pegasus, T5, GPT, LED, XLNet and graph neural models GNN and GAT during my recent research works. 
+</p>
+<p>
+  I worked as a software developer and was part of a software development team to develop software in the .NET platform. Additionally, I have been a part of different projects, for example, collaboration in Mitacs research projects and the design and development of a University Management System, which has given me the expertise of working in a team. 
+</p>
+<p>
+  While working as a Lecturer at the University of Lethbridge, Alberta, Canada, I gained experience communicating with diverse people from different cultures. With my interpersonal skills, I guided my students to be “Problem solvers” and “Critical thinkers.”
+</p>
+<p>
+  Languages that I prefer to work with are Python, C++, Java, C#, and JavaScript. I am also interested in cloud platforms, data science and deep learning models to develop ML models and find critical insights from the crazy data.
+</p>
               </div>}
             </TrackVisibility>
           </Col>
